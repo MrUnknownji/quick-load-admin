1bda310d3ac058ee61fe3816d073195d2ba0834d
MrUnknownji <sandeepkhati788@gmail.com>
2024-10-12 21:56:44 +0530

    Update route api's

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -1,7 +1,15 @@
-import { firebaseConfig } from "@/next-env";
+import { FIREBASE_API_KEY } from "@/next-env";
 import { initializeApp } from "firebase/app";
 import { getAuth, RecaptchaVerifier } from "firebase/auth";
-
+export const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: "moving-rolls-707e9.firebaseapp.com",
+  projectId: "moving-rolls-707e9",
+  storageBucket: "moving-rolls-707e9.appspot.com",
+  messagingSenderId: "350925667583",
+  appId: "1:350925667583:web:ba9e8dc9e9222e03a9a0ca",
+  measurementId: "G-NYD9J9ZTX9",
+};
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
